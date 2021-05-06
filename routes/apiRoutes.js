9ba28cc6539c72bf27a4f3c99d15ff1c6073a4df
Nathan_Wichmann <nathanwicker@hotmail.com>
2021-05-06 17:12:50 -0400

    updates

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,13 +1,14 @@
 const router = require('express').Router();
 const fs = require("fs");
-const noteList = JSON.parse(fs.readFileSync("db/db.json"));
 
 router.get('/notes', (req, res) => {
+    const noteList = JSON.parse(fs.readFileSync("db/db.json"));
     return res.json(noteList);
 });
 
 
 router.post('/notes', (req, res) => {
+    const noteList = JSON.parse(fs.readFileSync("db/db.json"));
     const newNote = {
         id: noteList[noteList.length - 1].id + 1,
         title: req.body.title,
@@ -27,9 +28,6 @@ router.delete('/notes/:id', (req, res) => {
     const noteId = parseInt(req.params.id)
     const noteList = JSON.parse(fs.readFileSync("db/db.json"));
     const newArray = noteList.filter(note => note.id !== noteId);
-
-
-
 
     fs.writeFileSync("db/db.json", JSON.stringify(newArray));
 
--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const path = require('path');
 //brings in the file sharing to read and write 
 const fs = require('fs');
 //brings in the notelist from index.js to read the file in the db.json
-const noteList = JSON.parse(fs.readFileSync("db/db.json"));
+
 //allows the files to be put in seperate folders 
 // const router = require('express').Router();
 
